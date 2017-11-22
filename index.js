85574d1e323c970a7044df46a4864b79d4d9c540
Lakshmi <lakshmi.nishthala@gmail.com>
2017-11-22 10:31:04 -0800

    Trying to store data in redis

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const server = require("http").createServer(app);
-const io = require("socket.io")(server);
-const bodyParser = require("body-parser");
-const handlebars = require("express-handlebars");
-let Promise = require("bluebird");
-const redis = require("redis");
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+const bodyParser = require('body-parser');
+const handlebars = require('express-handlebars');
+let Promise = require('bluebird');
+const redis = require('redis');
 Promise.promisifyAll(redis);
 let redisClient = redis.createClient();
 
 const hbs = handlebars.create({
-  defaultLayout: "main"
+	defaultLayout: 'main'
 });
 app.use(bodyParser.urlencoded({ extended: true }));
-app.engine("handlebars", hbs.engine);
-app.set("view engine", "handlebars");
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(
-  "/socket.io",
-  express.static(__dirname + "node_modules/socket.io-client/dist/")
+	'/socket.io',
+	express.static(__dirname + 'node_modules/socket.io-client/dist/')
 );
 
-app.get("/", (req, res) => {
-  res.render("index");
+app.get('/', (req, res) => {
+	res.render('index');
 });
 
 // look up how many objects there are in redis
@@ -37,26 +37,44 @@ app.get("/", (req, res) => {
 let totalMessages;
 let message;
 
-app.post("/", (req, res) => {
-  message = req.body["message"];
-  redisClient.keysAsync("*").then(keys => {
-    totalMessages = keys.length;
-    return totalMessages;
-  });
-.then(totalMessages => {
-  redisHMSET
-})
-  res.redirect("/");
+app.post('/', (req, res) => {
+	message = req.body['message'];
+	redisClient
+		.keysAsync('*')
+		.then(keys => {
+			totalMessages = keys.length;
+			return totalMessages;
+		})
+		.then(totalMessages => {
+			let p = redisClient.HMSETAsync(totalMessages, {
+				author: 'anonymous',
+				message: message,
+				id: totalMessages
+			});
+			return p;
+		})
+		.then(p => {
+			console.log(p);
+			console.log(totalMessages);
+			redisClient.hgetall(0, (err, data) => {
+				if (err) {
+					console.error(err);
+				} else {
+					console.log(data);
+				}
+			});
+		});
+	res.redirect('/');
 });
 
 app.use((err, req, res, next) => {
-  if (res.headersSent) {
-    return next(err);
-  }
-  if (err.stack) {
-    err = err.stack;
-  }
-  res.status(500).json({ error: err });
+	if (res.headersSent) {
+		return next(err);
+	}
+	if (err.stack) {
+		err = err.stack;
+	}
+	res.status(500).json({ error: err });
 });
 
 app.listen(3000);
